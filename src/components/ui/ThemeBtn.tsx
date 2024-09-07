25ed586d77f3049e96fa8e29c85a996425b2e010
Hassan <hassanshakil4569@gmail.com>
2024-09-08 01:05:49 +0500

    !problem: theme glitched

--- src/components/ui/ThemeBtn.tsx
+++ src/components/ui/ThemeBtn.tsx
@@ -1,17 +1,26 @@
 "use client"
 
 import { Sun, Moon } from 'lucide-react'
+import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import { Button } from './Button'
 
 const ThemeBtn = () => {
+    const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme()
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
+
     return (
-        <>
-            <Button variant="ghost" size="icon" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-                {theme === 'dark' ? <Sun className="h-[1.2rem] w-[1.2rem]" /> : <Moon className="h-[1.2rem] w-[1.2rem]" />}
-            </Button>
-        </>
+        <Button variant="ghost" size="icon" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+            {theme === 'dark' ? <Sun className="h-[1.2rem] w-[1.2rem]" /> : <Moon className="h-[1.2rem] w-[1.2rem]" />}
+        </Button>
     )
 }
 
